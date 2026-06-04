7c08058a8f76abb11a6df0fc75a7990cec966e0d
Stiben Rodriguez <s.rodriguez4926@gmail.com>
2026-06-03 23:00:09 -0500

    feat: create base application layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
+import { fileURLToPath } from 'node:url';
 
-
-// https://astro.build/config
 export default defineConfig({
-    vite: {
+  vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+        '@layouts': fileURLToPath(new URL('./src/layouts', import.meta.url)),
+      },
+    },
   },
 });
